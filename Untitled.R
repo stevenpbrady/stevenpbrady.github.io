a2107fa6b896d515d83c0ce1d1c3423b2f4f0f07
stevebrady <brady.steven@gmail.com>
2018-09-11 10:31:47 -0400

    Add 'lab expectations' page

---
+++ Untitled.R
@@ -0,0 +1,10 @@
+tempConvert <- function(startingScale, temp)
+  {
+ {
+  if (scale == "F") convertedTemp <- temp - 32 * 5/9 else
+  if (scale == "C") convertedTemp <- temp - 32 * 9/5
+ }
+  {
+  if (scale == "F") return{}
+  }
+}
